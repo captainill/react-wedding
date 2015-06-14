d5c2cfc272bafa643b7483815aa6c31a81e677a3
captainill <jonthomas0@gmail.com>
2015-06-14 13:26:27 -0700

    update prod to point to public

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -9,7 +9,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'assets'),
     filename: 'client.js',
-    publicPath: '/assets'
+    publicPath: '/public'
   },
   resolve: {
     extensions: [ '', '.js' ]
