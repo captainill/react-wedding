4de741e2334d21335307cccdbe235a3fc85ded10
JT <jt@ofitsownkind.com>
2015-06-15 18:49:08 -0700

    meta

--- src/components/Html.jsx
+++ src/components/Html.jsx
@@ -6,6 +6,7 @@
 import React from 'react';
 import DocumentTitle from "react-document-title";
 import Debug from 'debug';
+import Config from '../constants/Config';
 
 const debug = Debug('-------  SaladHacker');
 
@@ -29,8 +30,21 @@ var Html = React.createClass({
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />
+        <meta name="keywords" content={Config.keywords}/>
+        <meta name="description" content={Config.siteDescription}/>
+        <meta name="og:title" content={Config.siteTitle} />
+        <meta name="og:image" content={Config.siteImageUrl}/>
+        <meta name="og:site_name" content={Config.siteTitle} />
+        <meta name="og:url" content={Config.siteUrl} />
+        <meta name="og:description" content={Config.siteDescription} />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content={Config.siteUrl} />
+        <meta name="twitter:creator" content={Config.creatorTwitter} />
+        <meta name="twitter:title" content={Config.siteTitle} />
+        <meta name="twitter:description" content={Config.siteDescription} />
+        <meta name="twitter:image" content={Config.siteImageUrl}/>
+        <meta name="twitter:url" content={Config.site} />
         <title>{ DocumentTitle.rewind() }</title>
-        <link href='http://fonts.googleapis.com/css?family=Gentium+Book+Basic:400,400italic,700,700italic|Open+Sans:400,700' rel='stylesheet' type='text/css'/>
         <link href='http://fonts.googleapis.com/css?family=Vollkorn:400italic,700italic,400,700' rel='stylesheet' type='text/css'/>
         <link rel="stylesheet" href={this.props.bundlePath + "/style.css"} />
       </head>
--- src/constants/Config.js
+++ src/constants/Config.js
@@ -3,7 +3,13 @@ import keyMirror from "react/lib/keyMirror";
 const envrironment = process.env.NODE_ENV;
 
 const Config = {
-  imagePath: (envrironment === 'production') ? 'http://d7ut1n2771fln.cloudfront.net' : '/public'
+  imagePath: (envrironment === 'production') ? 'http://d7ut1n2771fln.cloudfront.net' : '/public',
+  creatorTwitter: '@captainill',
+  keywords: 'Love,HTML,JavaScript',
+  siteUrl: 'http://tayloranderica.com',
+  siteTitle: 'Taylor & Erica Crawford',
+  siteDescription: 'Happily Ever After...',
+  siteImageUrl: 'http://d7ut1n2771fln.cloudfront.net/images/DSC_0120.jpg'
 }
 
 export default Config;
