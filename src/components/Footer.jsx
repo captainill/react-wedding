303a1dd21a9dfd1d73cf79ef5d3267b240c63d4d
captainill <jonthomas0@gmail.com>
2015-06-22 16:00:16 -0700

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,7 +10,9 @@ export default class Footer extends React.Component{
   render() {
     return (
       <footer>
-        <p>Footer</p>
+        <p>Made by the <a href="http://ofitsownkind.com">Brother of the bride :P</a></p>
+        <p>You can <a href="mailto:jonthomas0@gmail.com">email</a> me if you have photos to add.</p>
+        <p>The site's <a href="https://github.com/captainill/react-wedding">source</a> is on Github</p>
       </footer>
     );
   }
