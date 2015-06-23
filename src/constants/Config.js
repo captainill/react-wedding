d2431a18b705d84284babddfa23600b3b0141e8b
captainill <jonthomas0@gmail.com>
2015-06-23 01:48:13 -0700

    update url

--- src/constants/Config.js
+++ src/constants/Config.js
@@ -6,7 +6,7 @@ const Config = {
   imagePath: (envrironment === 'production') ? 'http://d2dxb827r4wo7a.cloudfront.net' : '/assets',
   creatorTwitter: '@captainill',
   keywords: 'Love,HTML,JavaScript',
-  siteUrl: 'http://www.tayloranderica.com',
+  siteUrl: 'http://www.tayloranderica.com/',
   siteTitle: 'Taylor & Erica Crawford',
   siteDescription: 'Happily Ever After...',
   siteImageUrl: 'http://d2dxb827r4wo7a.cloudfront.net/images/DSC_0120.jpg'
